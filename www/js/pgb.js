7c0e5f671a7f117d5f5f0cba9de6bfd7bf234e54
unknown <you@example.com>
2018-03-11 00:46:01 +0100

    Dodanie wysylania sms

---
+++ www/js/pgb.js
@@ -0,0 +1,81 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function detectMovement() {
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 3000 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
+
+function showData() {
+	dane = 
+			'Barbara Czaicka'  + '\n' + 
+			'186128'  + '\n';
+
+	navigator.notification.alert(dane);
+}
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    navigator.notification.alert('Connection type: ' + states[networkState]);
+}
+
+function sendSMS() {
+		var number = document.getElementById('phoneNumber').value;
+        var message = document.getElementById('message').value;
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+		sms.send(number, message, options, success, error);
+}
